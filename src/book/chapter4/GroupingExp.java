396f4a5c3781519d965b507d7aaf3754be5f015a
erhan <erhan.turksel@btc-ag.com.tr>
2020-12-22 10:31:54 +0300

    added some exp

--- src/book/chapter4/GroupingExp.java
+++ src/book/chapter4/GroupingExp.java
@@ -2,6 +2,7 @@ package book.chapter4;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -19,14 +20,26 @@ public class GroupingExp {
 
         var ohMy2 = Stream.of("lions","tigers","bears");
 
-        Map<Boolean,List<String>> map2 = ohMy2.collect(Collectors.partitioningBy(s->s.length()<=5));
+        Map<Boolean, List<String>> map2 = ohMy2.collect(Collectors.partitioningBy(s -> s.length() <= 5));
         System.out.println(map2); //{false=[tigers], true=[lions, bears]}
 
 
-        var ohMy3 = Stream.of("lions","tigers","bears");
+        var ohMy3 = Stream.of("lions", "tigers", "bears");
 
-        Map<Integer,Long> map3 = ohMy3.collect(Collectors.groupingBy(String::length,Collectors.counting()));
+        Map<Integer, Long> map3 = ohMy3.collect(Collectors.groupingBy(String::length, Collectors.counting()));
         System.out.println(map3); // {5=2, 6=1}
+
+        //infinite stream
+        Stream<Integer> infiniteStream = Stream.iterate(1, i -> i + 2);
+        infiniteStream.limit(3).forEach(System.out::println);
+
+
+        Predicate<String> predicate = s -> s.length() > 3;
+        var iterateStream = Stream.iterate("-", s -> !s.isEmpty(), (s) -> s + s);
+        var b1 = iterateStream.noneMatch(predicate);
+        var b2 = iterateStream.anyMatch(predicate);
+        System.out.println(b1 + " " + b2);
+
 
     }
 }
